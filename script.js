7725b53377b759f9d23a46265043be6c16ad7b95
kelshardy <14024@coderacademy.edu.au>
2023-08-29 20:11:16 +1000

    Create webpage with Item Deletion

---
+++ script.js
@@ -0,0 +1,57 @@
+let favouriteMedia = [
+	"pokemon",
+	"digimon",
+	"supernatural",
+	"sherlock",
+	"doctor who",
+	"parks & recreation",
+	"marvel",
+	"harry potter",
+	"barbie",
+	"yugi-oh",
+	"oppenheimer",
+	"dragonball z"
+];
+
+function exampleHello(){
+    console.log("hello worlld");
+}
+
+function createListOfMedia(){
+
+    let rootUlNode = document.querySelector("ul");
+    // let exampleNode = document.getElementsByClassName("bananas")[0];
+    // let exampleNode = document.getElementById("mediaList");
+
+    // exampleNode[0]
+
+    favouriteMedia.forEach(mediaItem => {
+        // Create an element but do not display it yet
+        let newListItem = document.createElement("li");
+
+        // Assign some data to the element
+        newListItem.textContent = mediaItem;
+        newListItem.id = mediaItem;
+
+        // Create a button that will remove this item later
+        let removeItemButton = document.createElement("button");
+        
+        removeItemButton.onclick = (() => removeItemFromList(mediaItem));
+        
+        removeItemButton.textContent = "Remove item";
+
+        // Add the button to the list item
+        newListItem.appendChild(removeItemButton);
+
+        // Add the element to the visible page
+        rootUlNode.appendChild(newListItem);
+
+    });
+}
+
+function removeItemFromList(targetItem){
+    let targetItemNode = document.getElementById(targetItem);
+    if (targetItemNode){
+        targetItemNode.parentNode.removeChild(targetItemNode);
+    }
+}
